edb51c78812d6efebd5953699ba8b5d49d8ff830
reldwibr <uiler.reld02@gmail.com>
2022-07-21 13:24:05 -0300

    Second Code From Imersao Java

---
+++ src/APIs.java
@@ -0,0 +1,6 @@
+public class APIs {
+
+    public static String Top250Movies = "https://api.mocki.io/v2/549a5d8b";
+    public static String MostPopularMovies = "https://api.mocki.io/v2/549a5d8b/MostPopularMovies";
+
+}
