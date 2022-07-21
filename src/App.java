86e37193c4db954ec6fc91a4505e6ff3f7179c7c
reldwibr <uiler.reld02@gmail.com>
2022-07-21 13:40:17 -0300

    update 2

--- src/App.java
+++ src/App.java
@@ -1,78 +1,43 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println();
-        System.out.println("Digite 1 para Top 250 Filmes");
-        System.out.println();
-        System.out.println("Digite 2 para Filmes Mais Vistos");
-        System.out.println();
-        int choice = scan.nextInt();
-        switch (choice) {
-            case 1:
+        // fazer uma conexão HTTP e buscar os top 250 filmes
 
-                // Conexão HTTP com a API dos Top 250 Filmes
+        URI endereco = URI.create(APIs.Top250Movies);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
 
-                URI endereco250 = URI.create(APIs.Top250Movies);
-                var client = HttpClient.newHttpClient();
-                var request = HttpRequest.newBuilder(endereco250).GET().build();
-                HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-                String body = response.body();
-                
-                // Interpreta o Json para o Java
+        // extrair só os dados que interessam (titulo, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        
+        // exibir e manipular os dados
+        var stickers = new StickerMachine();
+        for (Map<String, String> filme : listaDeFilmes) {
 
-                var parser = new JsonParser();
-                List<Map<String, String>> listaDeFilmes = parser.parse(body);
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
 
-                // Exibe as Infos
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
 
-                for (Map<String, String> filme : listaDeFilmes) {
-                    System.out.println(filme.get("title"));
-                    System.out.println(filme.get("image"));
-                    System.out.println(filme.get("imDbRating"));
-                    System.out.println();
-                }
-                System.out.println("Nice!");
-                System.out.println();
-                break;
-            case 2:
-
-                // Conexão HTTP com a API dos Filmes Mais Vistos
-                URI enderecoPopular = URI.create(APIs.MostPopularMovies);
-                var client2 = HttpClient.newHttpClient();
-                var request2 = HttpRequest.newBuilder(enderecoPopular).GET().build();
-                HttpResponse<String> response2 = client2.send(request2, BodyHandlers.ofString());
-                String body2 = response2.body();
-                
-                // Interpreta o Json para o Java
-
-                var parser2 = new JsonParser();
-                List<Map<String, String>> listaDeFilmes2 = parser2.parse(body2);
-
-                // Exibe as Infos
-
-                for (Map<String, String> filme2 : listaDeFilmes2) {
-                    System.out.println(filme2.get("title"));
-                    System.out.println(filme2.get("image"));
-                    System.out.println(filme2.get("imDbRating"));
-                    System.out.println();
-                }
-                System.out.println("Nice!");
-                System.out.println();
-
-                break;
-
-            default:
-                System.out.println("Bugadinho BR");
+            stickers.cria(inputStream, nomeArquivo);
+            //
+            System.out.println(titulo);
+            System.out.println();
 
         }
     }
---
+++ src/StickerMachine.java
@@ -0,0 +1,58 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+public class StickerMachine {
+
+    public void cria(InputStream inputStream, String nomeArquivo) throws IOException, FontFormatException {
+
+        // Ler Imagem
+        /*
+         * Usando InputStream para ler uma imagem de um path da maquina local
+         * InputStream inputStream =
+         * new FileInputStream(new File("resources/images/MostPopularMovies_1.jpeg"));
+         *
+         * Acessando um InputStream por URL
+         * InputStream inputStream =
+         * new
+         * URL("https://imersao-java-apis.s3.amazonaws.com/MostPopularMovies_2.jpg").
+         * openStream();
+         */
+
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
+
+        // Cria nova imagem em memória com transparência e com tamanho novo
+
+        int largura = imagemOriginal.getWidth();
+        int altura = imagemOriginal.getHeight();
+        int novaAltura = altura + 200;
+        BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+
+        // Copiar imagem original para nova imagem (em memória)
+
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal, 0, 0, null);
+
+        // configurar fonte
+        InputStream stream = ClassLoader.getSystemClassLoader().getResourceAsStream("impact.ttf");
+        Font font = Font.createFont(Font.TRUETYPE_FONT, stream).deriveFont(100f);
+        //var fonte = new Font(Font., Font.BOLD, 100);
+        graphics.setColor(Color.YELLOW);
+        graphics.setFont(font);
+
+        // escrever uma frase na nova imagem
+
+        graphics.drawString("Filme Brabo!!", 150, novaAltura - 100);
+
+        // escrever a nova imagem em um arquivo
+        ImageIO.write(novaImagem, "png", new File(nomeArquivo));
+
+    }
+}
